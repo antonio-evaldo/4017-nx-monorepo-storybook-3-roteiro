41d2eebaf884712dabec0f75cc5a44a959837f33
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-11-05 16:14:07 -0300

    feat(storybook-host): adiciona stories para typography

--- libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
+++ libs/storybook-host/src/lib/Atoms/typography.component.stories.ts
@@ -3,6 +3,14 @@ import { TypographyComponent } from '@alfabit-alura-teste/typography';
 
 const meta: Meta<TypographyComponent> = {
   component: TypographyComponent,
+  render: (args) => ({
+    props: args,
+    template: `
+      <ab-typography ${argsToTemplate(args)}>
+        Text
+      </ab-typography>
+    `,
+  }),
 };
 
 export default meta;
@@ -11,14 +19,48 @@ type Story = StoryObj<TypographyComponent>;
 
 export const Title1: Story = {
   args: {
+    type: 'title1',
+  },
+};
+
+export const Title2: Story = {
+  args: {
+    ...Title1.args,
+    type: 'title2',
+  },
+};
+
+export const TextLarge: Story = {
+  args: {
     type: 'normal',
   },
-  render: (args) => ({
-    props: args,
+};
+
+export const TextMedium: Story = {
+  args: {
+    ...TextLarge.args,
+    size: 'md',
+  },
+};
+
+export const Title2WithMediumSize: Story = {
+  args: {
+    ...Title2.args,
+    size: 'md',
+  },
+};
+
+export const Paragraph: Story = {
+  args: {
+    ...TextLarge.args,
+  },
+  render: () => ({
     template: `
-      <ab-typography ${argsToTemplate(args)}>
-        Text
-      </ab-typography>
+      <p>
+        <ab-typography>
+          Texto do parágrafo
+        </ab-typography>
+      </p>
     `,
   }),
 };
